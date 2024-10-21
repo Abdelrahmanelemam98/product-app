8c2e1c8c9a6c16d150b61a72c682f02b575912db
Abdelrahman Elemam <74329630+Abdelrahmanelemam98@users.noreply.github.com>
2024-10-21 18:45:40 +0300

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,8 @@ module.exports = {
           "./src/app/product-list/product-list.component.ts",
         "./ProductDetailsComponent":
           "./src/app/product-details/product-details.component.ts",
+        "./CategoryListComponent":
+          "./src/app/category-list/category-list.component.ts",
       },
       // Uncomment and adjust if using remotes
       // remotes: {
